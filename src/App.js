0ce5172fb3fa7744338433862ad3c64e3c1c946a
SagarK107 <51955142+SagarK107@users.noreply.github.com>
2022-12-08 21:14:10 -0500

    first commit for todo list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function addTask(task) {
+  console.log(task);
+
+}
 
 function App() {
+
+  const [task,setTask] = useState("")
+  const [taskList,setTaskList] = useState([])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="addInput">
+      <input placeholder="Enter Task" onChange={(evt) => {setTask(evt.target.value)}}></input>
+      <Button variant="outline-primary" onClick={() => setTaskList((oldTaskList) =>  [...oldTaskList,task])}>Add Task</Button>
+      </div>
+      
+      <ul>
+      {        
+        taskList.map((task_)=> (
+          <div className="listItem">
+            <li key={task_.id}>{task_}</li>
+            <Button variant="outline-danger" class="deleteButton">Delete</Button>{' '}
+             
+          </div>
+          
+        ))
+
+      }
+      
+      </ul>
+      
     </div>
   );
 }
